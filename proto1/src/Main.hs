240355284dab72a80b594b6c210ddcbc8f0aa89d
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2019-12-31 22:30:42 +0100

    Implement unifier

--- proto1/src/Main.hs
+++ proto1/src/Main.hs
@@ -1,108 +1,120 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoMonadFailDesugaring #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Control.Monad.Identity
+import Control.Monad.Reader
 import Control.Monad.State
-import Data.HashMap.Strict (HashMap)
-import Data.Set (Set)
+import Data.Map (Map)
+import Data.Set (Set, difference, member, union)
 import Data.Text (Text)
 import Debug.Trace
-import qualified Data.HashMap.Strict as Map
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 
 
 -------------------------------------------------------------------------------
--- Name
+-- Var
 -------------------------------------------------------------------------------
 
-type Name = Text
+type Var = Text
 
 
-type Map = HashMap
+class Free a where
+    free :: a -> Set Var
+
+
+instance Free a => Free [a] where
+    free = foldr (union . free) mempty
 
 
 -------------------------------------------------------------------------------
 -- Eval
 -------------------------------------------------------------------------------
 
-type Env = Map Name Value
+type Env = Map Var Value
+
+
+type Eval = Reader Env
 
 
 emptyEnv :: Env
 emptyEnv = Map.empty
 
 
-data Value
-    = VInt !Int
-    | VBool !Bool
-    -- | VFloat !Double
-    -- | VUnit
-    -- | VChar !Char
-    -- | VString !Text
-    | VClosure !Name !Expr Env
+data Value 
+    = Int !Integer 
+    | Bool !Bool
+    -- | Int32 !Int32
+    -- | Int64 !Int64
+    -- | Float !Double
+    -- | Unit
+    -- | Char !Char
+    -- | String !Text
+    | Closure !Var !Expr Env
+    deriving (Eq)
 
 
 instance Show Value where
-    show (VInt n)    = show n
-    show (VBool b)   = show b
+    show (Int n)    = show n
+    show (Bool b)   = show b
     -- show (VFloat f)  = show f
     -- show (VChar c)   = show c
     -- show (VString s) = show s
     -- show VUnit       = "()"
-    show VClosure{}  = "<<closure>>"
+    show Closure{}  = "<<closure>>"
 
 
-binop :: Op -> Int -> Int -> Value
-binop Add a b = VInt (a + b)
-binop Mul a b = VInt (a * b)
-binop Sub a b = VInt (a - b)
-binop Eq a b = VBool (a == b)
+binop :: Op -> Integer -> Integer -> Value
+binop Add a b = Int (a + b)
+binop Mul a b = Int (a * b)
+binop Sub a b = Int (a - b)
+binop Eq a b = Bool (a == b)
 
 
-eval :: Env -> Expr -> Identity Value
-eval env = \case
+eval :: Expr -> Eval Value
+eval = \case 
     Var name -> do
-        let Just val = Map.lookup name env
+        Just val <- asks $ Map.lookup name
         pure val
 
     App fun arg -> do
-        VClosure name body env1 <- eval env fun
-        arg1 <- eval env arg
-        eval (Map.insert name arg1 env1) body
+        Closure name body env <- eval fun
+        arg' <- eval arg
+        let env' = Map.insert name arg' env
+        local (const env') (eval body)
 
-    Lam name body ->
-        pure (VClosure name body env)
+    Lam name body -> do
+        env <- ask 
+        pure (Closure name body env)
 
-    Let name expr body -> do
-        expr1 <- eval env expr
-        eval (Map.insert name expr1 env) body
+    Let name expr body -> 
+        eval (App (Lam name body) expr)
 
-    Lit (LInt int) ->
-        pure (VInt int)
-
-    Lit (LBool bool) ->
-        pure (VBool bool)
+    Lit val ->
+        pure val
 
     Fix expr ->
-        eval env (App expr (Fix expr))
+        eval (App expr (Fix expr))
 
     If cond true false -> do
-        VBool cond1 <- eval env cond
-        eval env (if cond1 then true else false)
+        Bool cond' <- eval cond
+        eval (if cond' then true else false)
 
     Op op a b -> do
-        VInt v1 <- eval env a
-        VInt v2 <- eval env b
-        pure (binop op v1 v2)
+        Int a' <- eval a
+        Int b' <- eval b
+        pure (binop op a' b')
 
 
-runEval :: Env -> Name -> Expr -> ( Value, Env )
+runEval :: Env -> Var -> Expr -> ( Value, Env )
 runEval env name expr =
     ( val, Map.insert name val env )
   where
-    val = runIdentity (eval env expr)
+    val = runReader (eval expr) env
 
 
 -------------------------------------------------------------------------------
@@ -110,35 +122,22 @@ runEval env name expr =
 -------------------------------------------------------------------------------
 
 data Expr
-    = Var !Name
+    = Var !Var
     | App !Expr !Expr
-    | Lam !Name !Expr
-    | Let !Name !Expr !Expr
-    | Lit !Lit
+    | Lam !Var !Expr
+    | Let !Var !Expr !Expr
+    | Lit !Value
     | Fix !Expr
     | If !Expr !Expr !Expr
     | Op !Op !Expr !Expr
     deriving (Show, Eq)
 
 
-data Lit
-    = LInt !Int
-    | LBool !Bool
---    | LFloat !Double 
---    | LUnit
---    | LChar !Char
---    | LString !Text
-    deriving (Show, Eq)
-
-
 data Op = Add | Sub | Mul | Eq
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 
-type Decl = ( Name, Expr )
-
-
-data Program = Program ![Decl] !Expr
+data Program = Program !(Map Var Expr) !Expr
     deriving (Show, Eq)
 
 
@@ -147,208 +146,237 @@ data Program = Program ![Decl] !Expr
 -------------------------------------------------------------------------------
 
 data Type
-    = TVar !Name
-    | TCon !Text
+    = TVar !Var
+--    | EVar !Int
+--    | TCon !Text
+    | TInt
+    | TBool
     | TArr !Type !Type
+    deriving (Show, Eq, Ord)
+
+
+instance Free Type where
+    free (TVar name)  = Set.singleton name
+    free (TArr t1 t2) = free t1 `union` free t2
+    free _            = mempty
+
+
+data Scheme = Forall !(Set Var) !Type
     deriving (Show, Eq)
 
 
-instance Substitutable Type where
-    free = undefined
-    apply = undefined
+instance Free Scheme where
+    free (Forall vars tau) = free tau `difference` vars
 
 
-data Scheme = Forall ![Name] !Type
+---------------------------------------------------------------------------------
+---- Typing environment (Context)
+---------------------------------------------------------------------------------
+
+newtype Context = Context (Map Var Scheme)
     deriving (Show, Eq)
 
 
-instance Substitutable Scheme where
-    free = undefined
-    apply = undefined
+instance Free Context where
+    free (Context env) = foldr (Set.union . free) mempty (Map.elems env)
 
 
-int :: Type
-int = TCon "Int"
+extend :: Var -> Scheme -> Context -> Context 
+extend name scheme (Context env) =
+    Context (Map.insert name scheme env)
 
 
-bool :: Type
-bool = TCon "Bool"
+---------------------------------------------------------------------------------
+---- Unification
+---------------------------------------------------------------------------------
 
-
--------------------------------------------------------------------------------
--- Typing environment (TypeEnv)
--------------------------------------------------------------------------------
-
-newtype TypeEnv = TypeEnv { types :: Map Name Scheme }
-    deriving (Show, Eq)
-
-
-instance Semigroup TypeEnv where
-    (<>) = union
-
-
-instance Monoid TypeEnv where
-    mempty = emptyTypeEnv
-
-
-instance Substitutable TypeEnv where
-    free = undefined
-    apply = undefined
-
-
-emptyTypeEnv :: TypeEnv
-emptyTypeEnv = TypeEnv Map.empty
-
-
-typeOf :: Name -> TypeEnv -> Maybe Scheme
-typeOf name (TypeEnv env) =
-    Map.lookup name env
-
-
-extend :: Name -> Scheme -> TypeEnv -> TypeEnv
-extend name scheme (TypeEnv env) =
-    TypeEnv (Map.insert name scheme env)
-
-
-remove :: Name -> TypeEnv -> TypeEnv
-remove name (TypeEnv env) =
-    TypeEnv (Map.delete name env)
-
-
-union :: TypeEnv -> TypeEnv -> TypeEnv
-union (TypeEnv a) (TypeEnv b) =
-    TypeEnv (Map.union a b)
-
-
-fromList :: [( Name, Scheme )] -> TypeEnv
-fromList =
-    TypeEnv . Map.fromList
-
-
-toList :: TypeEnv -> [( Name, Scheme )]
-toList (TypeEnv env) =
-    Map.toList env
-
-
--------------------------------------------------------------------------------
--- Unification
--------------------------------------------------------------------------------
-
-type Subst = Map Name Type
-
-
-class Substitutable a where
-    free :: a -> Set Name
-    apply :: Subst -> a -> a
-
-
-data InferState = InferState
+data UniState = UniState
     { count :: Int
     }
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 
-type Infer a = State InferState a
+newUniState :: UniState
+newUniState = UniState { count = 0 }
 
 
-getCount :: State InferState Int
-getCount = do
-    InferState{ count = count, .. } <- get
-    put InferState{ count = count + 1, .. }
+counterStep :: State UniState Int
+counterStep = do
+    UniState{ count = count, .. } <- get
+    put UniState{ count = count + 1, .. }
     pure count
 
 
-emptySubst :: Subst
-emptySubst = Map.empty
+type Unify = State UniState
 
 
-instantiate :: Scheme -> Infer Type
-instantiate (Forall vars _type) = 
-    undefined
+type Sub = Map Var Type
 
 
-generalize :: Type -> TypeEnv -> Scheme
-generalize _type env =
-    Forall vars _type
+instantiate :: Scheme -> Unify Type
+instantiate (Forall vars tau) = do
+    vars' <- mapM (const fresh) varsL
+    pure $ apply (Map.fromList (varsL `zip` vars')) tau
   where
-    vars = undefined
+    varsL = Set.toList vars
 
 
-inEnv :: Name -> Scheme -> Infer a -> Infer a
-inEnv name scheme =
-    undefined
+generalize :: Context -> Type -> Scheme
+generalize context tau =
+    Forall (free tau `difference` free context) tau
 
 
-infer :: Expr -> Infer Type
-infer = \case
+substitute :: Var -> Type -> Sub
+substitute = Map.singleton
+
+
+compose :: Sub -> Sub -> Sub
+compose sub1 sub2 = 
+    Map.map (apply sub1) sub2 `Map.union` sub1
+
+
+infer :: Context -> Expr -> Unify ( Sub, Type )
+infer context = \case
     Var name -> 
-        undefined
+        let 
+            Context env = context
+        in
+        case Map.lookup name env of
+            Nothing ->
+                fail "Unbound variable"
 
-    App fun arg -> do
-        funt <- infer fun
-        argt <- infer arg
-        t1 <- fresh
-        unify funt (TArr argt t1)
-        pure t1
+            Just scheme -> do
+                t1 <- instantiate scheme
+                pure ( mempty, t1 )
+
+    Lit (Int _) ->
+        pure ( mempty, TInt )
+
+    Lit (Bool _) ->
+        pure ( mempty, TBool )
 
     Lam name body -> do
-        undefined
-        --t1 <- fresh
-        --let tvar = Forall [] t1
-        --undefined (extend name tvar)
-        --t2 <- infer body
-        --pure (TArr t1 t2)
+        t <- fresh
+        let env' = extend name (Forall mempty t) context
+        ( s1, t1 ) <- infer env' body
+        pure ( s1, TArr (apply s1 t) t1 )
+
+    App fun arg -> do
+        t <- fresh
+        ( s1, t1 ) <- infer context fun
+        ( s2, t2 ) <- infer (apply1 s1 context) arg
+        s3 <- unify (apply s2 t1) (TArr t2 t)
+        pure ( s3 `compose` s2 `compose` s1, apply s3 t )
 
     Let name expr body -> do
-        t1 <- infer expr
-        undefined
-
-    Lit (LInt _ ) ->
-        pure int
-
-    Lit (LBool _) ->
-        pure bool
-
-    Fix expr ->
-        undefined
+        ( s1, t1 ) <- infer context expr
+        let env' = apply1 s1 context
+            t'   = generalize env' t1
+        ( s2, t2 ) <- infer (extend name t' env') body
+        pure ( s1 `compose` s2, t2 )
 
     If cond true false -> do
-        t1 <- infer cond
-        t2 <- infer true
-        t3 <- infer false
-        unify t1 bool
-        unify t2 t3
+        ( s1, t1 ) <- infer context cond
+        ( s2, t2 ) <- infer context true
+        ( s3, t3 ) <- infer context false
+        s4 <- unify t1 TBool
+        s5 <- unify t2 t3
+        pure ( s5 `compose` s4 `compose` s3 `compose` s2 `compose` s1, apply s5 t2 )
 
-    Op op a b -> 
-        undefined
+    Fix expr -> do
+        ( s1, t1 ) <- infer context expr
+        t <- fresh
+        s2 <- unify (TArr t t) t1
+        pure ( s2, apply s1 t )
 
-
-unify :: Type -> Type -> Infer Type
-unify fst snd =
-    case ( fst, snd ) of
-        ( TCon a, TCon b ) | a == b ->
-            pure fst
-
-        ( TArr a b, TArr a1 b1 ) -> do
-            t1 <- unify a a1
-            t2 <- unify b b1
-            pure (TArr t1 t2)
-
-        ( _, _ ) ->
-            error "Cannot unify"
+    Op op a b -> do
+        ( s1, t1 ) <- infer context a
+        ( s2, t2 ) <- infer context b
+        t <- fresh
+        s3 <- unify (TArr t1 (TArr t2 t)) (ops Map.! op)
+        pure ( s1 `compose` s2 `compose` s3, apply s3 t )
 
 
-occurs = undefined
+ops :: Map Op Type
+ops = Map.fromList 
+    [ ( Add, (TInt `TArr` (TInt `TArr` TInt)) )
+    , ( Mul, (TInt `TArr` (TInt `TArr` TInt)) )
+    , ( Sub, (TInt `TArr` (TInt `TArr` TInt)) )
+    , ( Eq, (TInt `TArr` (TInt `TArr` TBool)) )
+    ]
 
 
-names = fmap (\c -> Text.pack [c]) ['a' .. 'z'] ++ fmap (\n -> Text.pack ('a' : show n)) [1 .. ]
+apply2 :: Sub -> Scheme -> Scheme
+apply2 sub (Forall vars tau) =
+    Forall vars (apply sub' tau)
+  where
+    sub' = foldr Map.delete sub vars
 
 
-fresh :: Infer Type
+apply1 :: Sub -> Context -> Context
+apply1 sub (Context env) = 
+    Context (Map.map (apply2 sub) env)
+
+
+-- http://www.macs.hw.ac.uk/~yl55/UnPublished/ThesisMainText.pdf
+--
+unify :: Type -> Type -> Unify Sub
+unify = curry $ \case 
+    ( TBool, TBool ) -> 
+        pure mempty
+
+    ( TInt, TInt ) -> 
+        pure mempty
+
+    ( a `TArr` b, a1 `TArr` b1 ) -> do
+        t1 <- unify a a1
+        t2 <- unify (apply t1 b) (apply t1 b1)
+        pure (t2 `compose` t1)
+
+    ( TVar a, TVar b ) 
+        | a == b -> pure mempty
+
+    ( tau, TVar name ) -> 
+        unify (TVar name) tau
+
+    ( TVar name, tau ) 
+        | name `occursIn` tau -> fail "Infinite type"
+        | otherwise           -> pure (substitute name tau)
+
+    _ -> 
+        fail "Unification failed"
+
+  where
+    occursIn :: Var -> Type -> Bool
+    occursIn name tau = 
+        name `member` free tau
+
+
+apply :: Sub -> Type -> Type
+apply sub = \case 
+    TVar name ->
+        Map.findWithDefault (TVar name) name sub
+
+    TArr t1 t2 ->
+        TArr (apply sub t1) (apply sub t2)
+
+    tau ->
+        tau
+
+
+fresh :: Unify Type
 fresh = do
-    count <- getCount
-    let var = names !! count
+    count <- counterStep
+    let var = letters !! count
     pure (TVar var)
+
+
+letters = fmap Text.pack ( [1..] >>= flip replicateM ['a'..'z'] )
+
+
+runInfer :: Unify Type -> Type
+runInfer state =
+    evalState state newUniState 
 
 
 -------------------------------------------------------------------------------
@@ -357,23 +385,31 @@ main :: IO ()
 main =
     let
         expr1 =
-            Lam "x" (Op Add (Var "x") (Lit (LInt 1)))
+            Lam "x" (Op Add (Var "x") (Lit (Int 1)))
 
         expr2 =
-            App expr1 (Lit (LInt 3))
+            App expr1 (Lit (Int 3))
+
+        expr3 = -- let x = 4 in let x = 5 in x
+            Let "x" (Lit (Int 4)) (Let "x" (Lit (Int 5)) (Var "x"))
+
+        expr4 =
+            Let "x" (Lit (Int 4)) (Let "y" (Lit (Int 5)) (Var "x"))
 
         fact =
             Fix (Lam "f"
                 (Lam "n"
-                    (If (Op Eq (Lit (LInt 0)) (Var "n"))
-                        (Lit (LInt 1))
-                        (Op Mul (Var "n") (App (Var "f") (Op Sub (Var "n") (Lit (LInt 1))))))
+                    (If (Op Eq (Lit (Int 0)) (Var "n"))
+                        (Lit (Int 1))
+                        (Op Mul (Var "n") (App (Var "f") (Op Sub (Var "n") (Lit (Int 1))))))
                 )
             )
 
-        fact5 = App fact (Lit (LInt 5))
+        fact5 = App fact (Lit (Int 5))
 
-     in do
-     print (eval mempty fact5)
-     print (eval mempty expr1)
-     print (eval mempty expr2)
+   in do
+   print (runReader (eval fact5) mempty)
+   print (runReader (eval expr1) mempty)
+   print (runReader (eval expr2) mempty)
+   print (runReader (eval expr3) mempty)
+   print (runReader (eval expr4) mempty)
