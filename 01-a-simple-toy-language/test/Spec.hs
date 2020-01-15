7ee9f6a861f6755f4f6a97b9a5a71ebcf04e2585
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-01-15 13:01:52 +0300

    Write tests

--- 01-a-simple-toy-language/test/Spec.hs
+++ 01-a-simple-toy-language/test/Spec.hs
@@ -3,31 +3,52 @@
 import Control.Monad.Reader
 import Data.Text (Text)
 import Tau.Core
-import Tau.Eval
 import Tau.Core.Parser
+import Tau.Eval
+import Tau.Type
+import Tau.Type.Unify
 import Test.Hspec
 import Text.Megaparsec
 
+--
 
 program1 :: Text
 program1 = "let f = \\n -> if n == 0 then 1 else n * f (n-1) in f 5"
 
 
-program1_ast :: Ast
-Right program1_ast = parse ast "" program1
+program2 :: Text
+program2 = "\\x -> x + 1"
+
+--
+
+program_expr :: Text -> Expr
+program_expr src = toExpr program_ast
+  where
+    Right program_ast = parse ast "" src
 
 
-program1_expr :: Expr
-program1_expr = toExpr program1_ast
+typeOf :: Text -> Type
+typeOf src = apply sub t1
+  where
+    Right ( t1, cs ) = runInfer (infer (program_expr src))
+    Right sub = runSolver cs
 
 
-program1_val :: Value
-program1_val = runReader (eval program1_expr) mempty
+evald :: Text -> Value
+evald src = runReader (eval (program_expr src)) mempty
 
+--
 
 main :: IO ()
 main =
-    hspec $ do
+    hspec $ 
         describe "" $ do
-            it "" $ do
-                program1_val `shouldBe` (Tau.Core.Int 120)
+
+            it "" $ 
+                evald program1 `shouldBe` (Tau.Core.Int 120)
+
+            it "" $ 
+                typeOf program1 `shouldBe` tyInt
+
+            it "" $
+                typeOf program2 `shouldBe` (TyArr tyInt tyInt)
