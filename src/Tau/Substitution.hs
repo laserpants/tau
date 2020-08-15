6658bf8d0c9277ba4f6e328bb3d39002b6771d9c
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-08-16 01:24:25 +0300

    Refactor

--- src/Tau/Substitution.hs
+++ src/Tau/Substitution.hs
@@ -8,36 +8,34 @@ import Tau.Core
 import Tau.Pattern
 
 substitute :: Name -> Expr -> Expr -> Expr
-substitute name val = para alg where
-    alg :: ExprF (Expr, Expr) -> Expr
-    alg = \case
-        VarS var
-            | name == var -> val
-            | otherwise   -> varS var
+substitute name val = para $ \case
+    VarS var
+        | name == var -> val
+        | otherwise   -> varS var
 
-        LamS var (expr, body)
-            | name == var -> lamS var expr
-            | otherwise   -> lamS var body
+    LamS var (expr, body)
+        | name == var -> lamS var expr
+        | otherwise   -> lamS var body
 
-        AppS exprs ->
-            appS (snd <$> exprs)
+    AppS exprs ->
+        appS (snd <$> exprs)
 
-        LitS prim ->
-            litS prim
+    LitS prim ->
+        litS prim
 
-        LetS var body expr ->
-            let get = if name == var then fst else snd
-             in letS var (get body) (get expr)
+    LetS var body expr ->
+        let get = if name == var then fst else snd
+         in letS var (get body) (get expr)
 
-        IfS (_, cond) (_, true) (_, false) ->
-            ifS cond true false
+    IfS (_, cond) (_, true) (_, false) ->
+        ifS cond true false
 
-        CaseS (_, expr) clss ->
-            caseS expr (fun <$> clss) where
-                fun (p, e) = (p, if name `elem` getVars p then fst e else snd e)
+    CaseS (_, expr) clss ->
+        caseS expr (fun <$> clss) where
+            fun (p, e) = (p, if name `elem` getVars p then fst e else snd e)
 
-        OpS op ->
-            opS (snd <$> op)
+    OpS op ->
+        opS (snd <$> op)
 
-        AnnS expr ty ->
-            undefined  -- TODO
+    AnnS expr ty ->
+        undefined  -- TODO
