680d2e60c301f922997f288324256f554931c9c4
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-09-10 19:10:39 +0300

    Refactor

--- temp/test/Spec.hs
+++ temp/test/Spec.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Either
 import Test.Hspec
 import Tau.Type
 
+failUnifyTypes :: Type -> Type -> SpecWith ()
+failUnifyTypes t1 t2 = do
+    let result = unify t1 t2
+    describe "The types t1 and t2" $
+
+        it "✗ fails to unify" $
+            isLeft result
+
 succeedUnifyTypes :: Type -> Type -> SpecWith ()
 succeedUnifyTypes t1 t2 = do
-
     let result = unify t1 t2
-
     describe "The types t1 and t2" $ do
 
         it "✔ yields a substitution" $
@@ -16,10 +23,10 @@ succeedUnifyTypes t1 t2 = do
             let Right sub = result
             apply sub t1 == apply sub t2
 
-
 testTypeUnification :: SpecWith ()
-testTypeUnification = 
+testTypeUnification = do
     succeedUnifyTypes (arrT (varT "a") (varT "b")) (arrT tInt tInt)
+    failUnifyTypes (arrT (varT "a") (varT "a")) (arrT tInt tBool)
 
 main :: IO ()
 main =
