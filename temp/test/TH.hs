3ea624243be65f3d41a169db3f975844939d8ccd
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-09-13 23:55:46 +0300

    Refactor

--- temp/test/TH.hs
+++ temp/test/TH.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuasiQuotes     #-}
 module TH where
 
 import Data.Either (fromRight)
@@ -8,17 +7,20 @@ import Data.Text (Text)
 import Language.Haskell.TH (Q, Exp, runQ)
 import Tau.Parser
 
+forceParser :: Parser a -> Text -> a
+forceParser parser = fromRight (error "Does not parse") . runParser parser ""
+
 mkExpr :: Text -> Q Exp
-mkExpr input = runQ [| fromRight (error "Does not parse") (runParser expr "" input) |]
+mkExpr input = runQ [| forceParser expr input |]
 
 mkScheme :: Text -> Q Exp
-mkScheme input = runQ [| fromRight (error "Does not parse") (runParser scheme "" input) |]
+mkScheme input = runQ [| forceParser scheme input |]
 
 mkType :: Text -> Q Exp
-mkType input = runQ [| fromRight (error "Does not parse") (runParser type_ "" input) |]
+mkType input = runQ [| forceParser type_ input |]
 
 mkKind :: Text -> Q Exp
-mkKind  input = runQ [| fromRight (error "Does not parse") (runParser kind "" input) |]
+mkKind  input = runQ [| forceParser kind input |]
 
 mkPattern :: Text -> Q Exp
-mkPattern input = runQ [| fromRight (error "Does not parse") (runParser parsePattern "" input) |]
+mkPattern input = runQ [| forceParser parsePattern input |]
