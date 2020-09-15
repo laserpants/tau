17aa6442bcad0201acde8f8d496cb85404e7b0fb
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-09-16 00:45:30 +0300

    Refactor

--- src/Tau/Parser.hs
+++ src/Tau/Parser.hs
@@ -116,21 +116,22 @@ operator =
       ]
     , [ Prefix (notS <$ (symbol "not" *> spaces))
       ]
-    , [ InfixN (eqS  <$ symbol "==")
-      , InfixN (neqS <$ symbol "/=")
-      , InfixN (ltS  <$ symbol "<")
-      , InfixN (gtS  <$ symbol ">")
-      ]
     , [ InfixL (mulS <$ symbol "*")
       , InfixL (divS <$ symbol "/")
       ]
     , [ InfixL (addS <$ symbol "+")
       , InfixL (subS <$ symbol "-")
       ]
-    , [ InfixN (orS  <$ symbol "||")
-      , InfixN (andS <$ symbol "&&")
-      ]
     , [ InfixR (cons <$ symbol "::")
+      ]
+    , [ InfixN (eqS  <$ symbol "==")
+      , InfixN (neqS <$ symbol "/=")
+      , InfixN (ltS  <$ symbol "<")
+      , InfixN (gtS  <$ symbol ">")
+      ]
+    , [ InfixN (andS <$ symbol "&&")
+      ]
+    , [ InfixN (orS  <$ symbol "||")
       ]
     ]
 
