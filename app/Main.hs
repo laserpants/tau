f16c5417f71e932cacb57b592490f6f7aed272ce
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-09-15 20:32:03 +0300

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
-main :: IO ()
-main = pure ()
+import Control.Exception.Base
+
+main = toTry `catch` handler
+
+toTry = do
+    print "hi"
+    print (show (3 `div` 0))
+    print "hi"
+
+handler :: ArithException -> IO ()
+handler DivideByZero = putStrLn "Divide by Zero!"
+handler err = putStrLn "Some other error..."
+
+--main :: IO ()
+--main = pure ()
