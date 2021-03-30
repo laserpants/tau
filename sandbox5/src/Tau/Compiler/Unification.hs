d2888808718fff94a0472f2b44769e3bacb1b390
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2021-03-30 23:00:00 +0300

    Refactor

--- sandbox5/src/Tau/Compiler/Unification.hs
+++ sandbox5/src/Tau/Compiler/Unification.hs
@@ -2,6 +2,7 @@
 module Tau.Compiler.Unification where
 
 import Control.Monad.Except
+import Data.Function ((&))
 import Tau.Compiler.Substitution
 import Tau.Tool
 import Tau.Type
@@ -16,23 +17,43 @@ bind name kind ty
     | otherwise                               = pure (name `mapsTo` ty)
 
 unify :: (MonadError Error m) => Type -> Type -> m TypeSubstitution
-unify t u = undefined
+unify t u
+    | isRow t && isRow u                      = unifyRowTypes t u
+    | otherwise                               = fn (project t) (project u)
+  where
+    fn (TArr t1 t2) (TArr u1 u2)              = unifyPairs (t1, t2) (u1, u2)
+    fn (TApp t1 t2) (TApp u1 u2)              = unifyPairs (t1, t2) (u1, u2)
+    fn (TVar kind name) _                     = bind name kind u
+    fn _ (TVar kind name)                     = bind name kind t
+    fn _ _ | t == u                           = pure mempty
+    fn _ _                                    = throwError "Cannot unify"
 
 match :: (MonadError Error m) => Type -> Type -> m TypeSubstitution
-match t u = undefined
+match t u = fn (project t) (project u)
+  where
+    fn (TArr t1 t2) (TArr u1 u2)              = matchPairs (t1, t2) (u1, u2)
+    fn (TApp t1 t2) (TApp u1 u2)              = matchPairs (t1, t2) (u1, u2)
+    fn (TVar k name) _ | k == kindOf u        = pure (name `mapsTo` u)
+    fn _ _ | t == u                           = pure mempty
+    fn _ _                                    = throwError "Cannot match"
 
 unifyPairs :: (MonadError Error m) => (Type, Type) -> (Type, Type) -> m TypeSubstitution
 unifyPairs (t1, t2) (u1, u2) = do
-    undefined
+    sub1 <- unify t1 u1
+    sub2 <- unify (apply sub1 t2) (apply sub1 u2)
+    pure (sub2 <> sub1)
 
 matchPairs :: (MonadError Error m) => (Type, Type) -> (Type, Type) -> m TypeSubstitution
 matchPairs (t1, t2) (u1, u2) = do
-    undefined
+    sub1 <- match t1 u1
+    sub2 <- match t2 u2
+    merge sub1 sub2 & maybe (throwError "Merge failed") pure
 
 unifyRowTypes :: (MonadError Error m) => Type -> Type -> m TypeSubstitution
-unifyRowTypes t u = 
+unifyRowTypes t u =
     undefined
 
 matchRowTypes :: (MonadError Error m) => Type -> Type -> m TypeSubstitution
-matchRowTypes t u = 
+matchRowTypes t u =
     undefined
+
