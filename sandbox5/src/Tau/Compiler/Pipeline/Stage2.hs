24e770682f9e9070ea464438c1d2b6af44c2dffd
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2021-05-16 04:16:03 +0300

    Refactor

--- sandbox5/src/Tau/Compiler/Pipeline/Stage2.hs
+++ sandbox5/src/Tau/Compiler/Pipeline/Stage2.hs
@@ -1,2 +1,5 @@
 module Tau.Compiler.Pipeline.Stage2 where
 
+type SourceExpr t = Expr t t t t t t t t t Void Void Void Void Void Void
+    (ProgBinding t) [ProgPattern t] (SimplifiedClause t (ProgPattern t))
+
