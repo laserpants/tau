4beb52eb4bbf86bfe7161f4b42377a4e47882c58
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2021-05-16 12:39:43 +0300

    Refactor

--- sandbox5/src/Tau/Compiler/Pipeline/Stage2.hs
+++ sandbox5/src/Tau/Compiler/Pipeline/Stage2.hs
@@ -1,9 +1,18 @@
 module Tau.Compiler.Pipeline.Stage2 where
 
+import Tau.Compiler.Error
 import Tau.Compiler.Pipeline
 import Tau.Lang
+import Tau.Prog
 import Tau.Tool
+import Tau.Type
 
 type SourceExpr t = Expr t t t t t t t t t Void Void Void Void Void Void
     (ProgBinding t) [ProgPattern t] (SimplifiedClause t (ProgPattern t))
 
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+translate 
+  :: SourceExpr (TypeInfoT [Error] (Maybe Type))
+  -> SourceExpr (Maybe Type)
+translate = undefined
