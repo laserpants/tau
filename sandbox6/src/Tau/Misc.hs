30f71cfbb25b6a8b1300bda5eb2d9229c541e872
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2021-08-06 01:08:04 +0300

    Refactor

--- sandbox6/src/Tau/Misc.hs
+++ sandbox6/src/Tau/Misc.hs
@@ -334,6 +334,71 @@ deriving instance (Traversable e2, Traversable e4) =>
     Traversable (ExprF t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11 t12 t13 t14 t15 t16 t17 e1 e2 e3 e4)
 
 -------------------------------------------------------------------------------
+-- Core
+-------------------------------------------------------------------------------
+
+type CMatrix a = [([Name], a)]
+
+data CoreF a
+    = CVar Name                 -- ^ Variable
+    | CLit Prim                 -- ^ Primitive value
+    | CApp [a]                  -- ^ Function application
+    | CLet Name a a             -- ^ Let expression
+    | CLam Name a               -- ^ Lambda abstraction
+    | CIf  a ~a ~a              -- ^ If-clause
+    | CPat a (CMatrix a)        -- ^ Pattern match clause matrix
+
+-- | Core language expression used for interpreted program evaluation and code 
+-- generation
+type Core = Fix CoreF
+
+-------------------------------------------------------------------------------
+
+-- Type class instances for Core
+
+deriving instance (Show a) => Show (CoreF a)
+deriving instance (Eq   a) => Eq   (CoreF a)
+deriving instance (Ord  a) => Ord  (CoreF a)
+
+deriveShow1 ''CoreF
+deriveEq1   ''CoreF
+deriveOrd1  ''CoreF
+
+deriving instance Functor     CoreF
+deriving instance Foldable    CoreF
+deriving instance Traversable CoreF
+
+-------------------------------------------------------------------------------
+
+cVar :: Name -> Core
+cVar = embed1 CVar
+{-# INLINE cVar #-}
+
+cLit :: Prim -> Core
+cLit = embed1 CLit
+{-# INLINE cLit #-}
+
+cApp :: [Core] -> Core
+cApp = embed1 CApp
+{-# INLINE cApp #-}
+
+cLet :: Name -> Core -> Core -> Core
+cLet = embed3 CLet
+{-# INLINE cLet #-}
+
+cLam :: Name -> Core -> Core
+cLam = embed2 CLam
+{-# INLINE cLam #-}
+
+cIf :: Core -> Core -> Core -> Core
+cIf = embed3 CIf
+{-# INLINE cIf #-}
+
+cPat :: Core -> CMatrix Core -> Core
+cPat = embed2 CPat
+{-# INLINE cPat #-}
+
+-------------------------------------------------------------------------------
 -- Prog
 -------------------------------------------------------------------------------
 
